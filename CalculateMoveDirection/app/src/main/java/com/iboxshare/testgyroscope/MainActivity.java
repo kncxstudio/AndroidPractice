b9109a136a9c5073755ec403f6943efd64c5330b
Junqin Wang <kncx@qq.com>
2016-11-10 00:15:22 +0800

    矫正加速度     添加用户手持模式判断
    矫正加速度     添加用户手持模式判断

--- CalculateMoveDirection/app/src/main/java/com/iboxshare/testgyroscope/MainActivity.java
+++ CalculateMoveDirection/app/src/main/java/com/iboxshare/testgyroscope/MainActivity.java
@@ -17,11 +17,21 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
     private Context context = this;
-    private TextView xTV,yTV,zTV,gxTV,gyTV,gzTV;
-    private Sensor aSensor,gSensor;
+    private TextView xTV,yTV,zTV
+            ,gxTV,gyTV,gzTV
+            ,gyroscopeXTV,gyroscopeYTV,gyroscopeZTV
+            ,showDirectionTV;
+    private Sensor aSensor,gSensor,gyroscopeSensor;
     private SensorManager sensorManager;
+    static double ax,ay,az
+            ,gx,gy,gz
+            ,gyroscopeX,gyroscopeY,gyroscopeZ;
 
-    static double ax,ay,az,gx,gy,gz;
+
+    //Flags
+    public static String currentDirection = "null";
+    public static int currentUserHoldMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    /**
+     * 绑定View
+     */
     void bindView(){
         xTV = (TextView) findViewById(R.id.loc_x);
         yTV = (TextView) findViewById(R.id.loc_y);
@@ -37,12 +50,50 @@ public class MainActivity extends AppCompatActivity {
         gxTV = (TextView) findViewById(R.id.g_x);
         gyTV = (TextView) findViewById(R.id.g_y);
         gzTV = (TextView) findViewById(R.id.g_z);
+        gyroscopeXTV = (TextView) findViewById(R.id.gyroscope_x);
+        gyroscopeYTV = (TextView) findViewById(R.id.gyroscope_y);
+        gyroscopeZTV = (TextView) findViewById(R.id.gyroscope_z);
+        showDirectionTV = (TextView) findViewById(R.id.show_direction_tv);
     }
 
+    /**
+     * 初始化
+     */
     void init(){
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         aSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         gSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                //X轴 左右
+                //Log.e(TAG,""+event.values[0]);
+                gyroscopeX = event.values[0];
+                //Y轴 前后
+                //Log.e(TAG,""+event.values[1]);
+                gyroscopeY = event.values[1];
+                //Z轴 上下
+                //Log.e(TAG,""+event.values[2]);
+                gyroscopeZ = event.values[2];
+
+
+                gyroscopeXTV.setText("gyrosX: "+ gyroscopeX);
+                gyroscopeYTV.setText("gyrosY: "+ gyroscopeY);
+                gyroscopeZTV.setText("gyrosZ: "+ gyroscopeZ);
+            }
+
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        },gyroscopeSensor,SensorManager.SENSOR_DELAY_GAME);
+
+
+
+        //注册加速仪监听器
         sensorManager.registerListener(new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
@@ -71,9 +122,10 @@ public class MainActivity extends AppCompatActivity {
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
             }
-        },aSensor,SensorManager.SENSOR_DELAY_UI);
+        },aSensor,SensorManager.SENSOR_DELAY_GAME);
 
 
+        //注册重力加速度监听器
         sensorManager.registerListener(new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
@@ -95,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
             }
-        },gSensor,SensorManager.SENSOR_DELAY_UI);
+        },gSensor,SensorManager.SENSOR_DELAY_GAME);
     }
 
 
@@ -103,17 +155,95 @@ public class MainActivity extends AppCompatActivity {
      * 通过ax,ay,zy,gx,gy,gz计算设备移动方向
      */
     void calculateDirection(){
-        //Log.e(TAG,"x方向加速度：" + (ax - gx));
-        if (ax - gx > 5.0){
-            Log.e(TAG,"向左移动");
 
-            if (az - gz > 3.0){
-                Log.e(TAG,"向左下方移动");
-            }else if(az - gz < -3.0){
-                Log.e(TAG,"向左上方移动");
+
+        //矫正加速度
+        ax = 0.8 * ax + 0.2 * gx;
+        ay = 0.8 * ay + 0.2 * gy;
+        az = 0.8 * az + 0.2 * gz;
+
+
+        //判断玩家手机握持方式
+        currentUserHoldMode = calculateUserHoldMode();
+
+
+        if (az - gz > 5.0){
+            //Log.e(TAG,"向下移动");
+
+            switch (currentUserHoldMode){
+                case 0:
+                    currentDirection = "向下移动";
+                    break;
+                case 1:
+                    currentDirection = "向右移动";
+                    break;
+                case 2:
+                    currentDirection = "向左移动";
+                    break;
+            }
+        }else if(az - gz < -5.0){
+            //Log.e(TAG,"向上移动");
+            switch (currentUserHoldMode){
+                case 0:
+                    currentDirection = "向上移动";
+                    break;
+                case 1:
+                    currentDirection = "向左移动";
+                    break;
+                case 2:
+                    currentDirection = "向右移动";
+                    break;
+            }
+        }else if (ax - gx > 5.0){
+            //Log.e(TAG,"向左移动");
+            switch (currentUserHoldMode){
+                case 0:
+                    currentDirection = "向左移动";
+                    break;
+                case 1:
+                    currentDirection = "向下移动";
+                    break;
+                case 2:
+                    currentDirection = "向上移动";
+                    break;
             }
         }else if(ax - gx < -5.0){
-            Log.e(TAG,"向右移动");
+            //Log.e(TAG,"向右移动");
+            switch (currentUserHoldMode){
+                case 0:
+                    currentDirection = "向右移动";
+                    break;
+                case 1:
+                    currentDirection = "向上移动";
+                    break;
+                case 2:
+                    currentDirection = "向下移动";
+                    break;
+            }        }
+
+        showDirectionTV.setText(currentDirection);
+    }
+
+
+    /**
+     * 计算用户握持方式
+     * @return
+     */
+    int calculateUserHoldMode(){
+        int mode = 0;
+        if (gx < 4.0 && gx > -4.0){
+            //屏幕朝上标准握持方式
+            //Log.e(TAG,"标准握持方式");
+            mode = 0;
+        }else if(gx > 4.0){
+            //右手自然握持方式
+            //Log.e(TAG,"右手自然握持方式");
+            mode = 1;
+        }else if (gx < -4.0){
+            //左手自然握持方式
+            //Log.e(TAG,"左手自然握持方式");
+            mode = 2;
         }
+        return mode;
     }
 }
